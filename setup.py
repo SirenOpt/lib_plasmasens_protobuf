a65e8e9edc063ab45898bab6e3437dee32ffac77
Michael Craig Jr <michael@michaelcraig.group>
2024-08-14 10:39:56 -0700

    [version] bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='lib_plasmasens_protobuf',
-    version='0.2.2',
+    version='1.5.1',
     packages=find_packages(),
     install_requires=[
         'grpcio',
